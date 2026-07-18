fd903365d68e52f1679a5d79607eb237a7e42577
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-07-18 14:16:34 +0500

    5: FilterBar Comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import FilterBar from "./components/FilterBar"
 
 function App() {
   return (
-    <div className="bg-[#FEFBF5] min-h-screen ">
-      <Navbar/>
-      <HeroSection/>
+    <div className="bg-white min-h-screen">
+      <div className="bg-[#FEFBF5] pb-16 ">
+        <Navbar />
+        <HeroSection />
+      </div>
+      <FilterBar/>
     </div>
   );
 }
---
+++ src/components/FilterBar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { MapPin, Home, CircleDollarSign } from "lucide-react";
+
+function FilterBar() {
+  return (
+    <div className="max-w-5xl bg-[#E4D1C6] mx-auto rounded-2xl p-6 flex justify-between -mt-14 relative z-10 gap-6  shadow-sm">
+      {/* Location Box  */}
+      <div className="bg-[#FBF5F1] flex items-center justify-between px-4 py-3 rounded-lg w-full relative">
+        <input
+          type="text"
+          placeholder="Location"
+          className="bg-transparent outline-none w-full text-[#2B1B12] placeholder:text-[#2B1B12] font-bold placeholder:font-bold"
+        />
+        <MapPin size={18} className="text-[#2B1B12] shrink-0" />
+      </div>
+
+      {/* Type Select DropDown  */}
+      <div className="bg-[#FBF5F1] flex items-center justify-between p-6 w-full rounded-lg relative">
+        <select
+          defaultValue=""
+          className="outline-none bg-transparent w-full text-sm appearance-none font-bold text-sm cursor-pointer"
+        >
+          <option value="" disabled>
+            Type
+          </option>
+          <option value="apartment">apartment</option>
+          <option value="villa">villa</option>
+          <option value="cottage">cottage</option>
+          <option value="mansion">mansion</option>
+        </select>
+        <Home
+          size={18}
+          className="text-[#2B1B12] shrink-0 absolute right-4 pointer-events-none"
+        />
+      </div>
+
+      {/* Price Box  */}
+      <div className="bg-[#FEFBF5] flex items-center justify-between px-4 py-3 rounded-lg w-full relative">
+        <select
+          defaultValue=""
+          className="bg-transparent outline-none w-full text-[#2B1B12] font-bold text-sm appearance-none cursor-pointer"
+        >
+          <option value="" disabled>
+            Price Range
+          </option>
+          <option value="100-300">$100k - $300k</option>
+          <option value="300-500">$300k - $500k</option>
+          <option value="500-1m">$500k - $1M</option>
+          <option value="1m+">$1M+</option>
+        </select>
+        <CircleDollarSign
+          size={18}
+          className="text-[#2B1B12] shrink-0 absolute right-4 pointer-events-none"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default FilterBar;
